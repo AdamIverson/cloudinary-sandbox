e9ca92e0c7eb7dc7f5ef253930156f24c3c0842b
Adam Iverson <aciverso@gmail.com>
2022-05-24 13:02:04 -0500

    back to base

--- server/routes/upload.router.js
+++ server/routes/upload.router.js
@@ -2,9 +2,9 @@ const express = require("express");
 // const pool = require("../modules/pool");
 const router = express.Router();
 
-router.post('/photos/upload', fileUpload.array('image', 5), function (req, res, next) {
-      console.log("Images ", req.file);  
-})
+// router.post('/photos/upload', fileUpload.array('image', 5), function (req, res, next) {
+//       console.log("Images ", req.file);  
+// })
 
 
 
