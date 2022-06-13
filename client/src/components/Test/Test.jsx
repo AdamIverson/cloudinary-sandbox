1026f40ee1db65a61ac004b085745f11291ab022
Adam Iverson <aciverso@gmail.com>
2022-06-13 07:24:04 -0500

    cloudinary installing, image data logging in server

--- client/src/components/Test/Test.jsx
+++ client/src/components/Test/Test.jsx
@@ -1,11 +1,60 @@
 import React, { useState, useEffect } from "react";
-// require('dotenv').config;
+
 function Test() {
+  const [fileInputState, setFileInputState] = useState("");
+  const [selectedFile, setSelectedFile] = useState("");
+  const [previewSource, setPreviewSource] = useState("");
 
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    previewFile(file);
+  };
 
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPreviewSource(reader.result);
+    }
+  }
+
+  const uploadImage = async (jazz) => {
+     console.log('JAZZ: ', jazz);
+     try {
+      await fetch('/api/upload', {
+        method: 'POST',
+        body: JSON.stringify({data: jazz}),
+        headers: {'Content-type': 'application/json'}
+      }
+      )
+     } catch (error) {
+      console.error(error)
+     }
+  }
+
+  const handleSubmitFile = (e) => {
+    e.preventDefault();
+    if(!previewSource) return;
+    uploadImage(previewSource);
+  }
   return (
     <div>
       <h1>test</h1>
+      <form onSubmit={handleSubmitFile}>
+        <input
+          type="file"
+          name="image"
+          onChange={handleFileInputChange}
+          value={fileInputState}
+        />
+        <button type="submit"></button>
+      </form>
+      {previewSource && (
+        <img 
+        src={previewSource}
+        alt="chosen"
+        />
+      )}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const { cloudinary } = require('./utils/cloudinary');
 require('dotenv').config();
 
 const app = express();
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.post('/api/upload', (req, res) => {
+  try {
+    const fileStr = req.body.data;
+    console.log(fileStr);
+  } catch (error) {
+      console.error(error)
+  }
+})
+
 const testRouter = require('./routes/test.router');
-const uploadRouter = require('./routes/upload.router');
 
 app.use('/api/test', testRouter);
-app.use('/api/upload', uploadRouter);
-
+// 
 // app.get("/api", (req, res) => {
 //   res.json({ message: "Hello from server!" });
 // });
